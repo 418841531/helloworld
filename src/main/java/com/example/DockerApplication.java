4470d5c7bdcc9d249c289d796f790c5b4e81f1de
Administrator <Administrator@USER-20180809JC>
2019-10-30 15:03:51 +0800

    1

---
+++ src/main/java/com/example/DockerApplication.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class DockerApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApplication.class, args);
+	}
+
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "hello world 1";
+	}
+	
+	
+}
